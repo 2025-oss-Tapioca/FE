b515a347bd2f08ebd4d4e185eddb62d929dd3034
a1rhun <83606547+a1rhun@users.noreply.github.com>
2025-08-06 20:18:17 +0900

    feat/home 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,20 +11,22 @@ const Home = () => {
 
   return (
     <div className="container">
-      <SideBar
-        className="side-bar"
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-      />
-      <main className="main-content">
-        {activeTab === "대시보드" && <h2 className="page-title">대시보드</h2>}
-        {activeTab === "서버관리" && <ServerManagement />}
-        {activeTab === "개발도구" && <DevTools />}
-        {activeTab === "성능 테스트" && <PerformanceTest />}
-        {activeTab === "로그 모니터링" && (
-          <h2 className="page-title">로그 모니터링</h2>
-        )}
-      </main>
+      <div className="layout-inner">
+        <SideBar
+          className="side-bar"
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
+        <main className="main-content">
+          {activeTab === "대시보드" && <h2 className="page-title">대시보드</h2>}
+          {activeTab === "서버관리" && <ServerManagement />}
+          {activeTab === "개발도구" && <DevTools />}
+          {activeTab === "성능 테스트" && <PerformanceTest />}
+          {activeTab === "로그 모니터링" && (
+            <h2 className="page-title">로그 모니터링</h2>
+          )}
+        </main>
+      </div>
     </div>
   );
 };
