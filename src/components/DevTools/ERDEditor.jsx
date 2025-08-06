a1ebcc4f7ed9b4e222e99474073ec9c974fe532e
son0131 <sonsh0131@gmail.com>
2025-08-07 02:54:01 +0900

    ERD 작성 박스 제작

--- src/components/DevTools/ERDEditor.jsx
+++ src/components/DevTools/ERDEditor.jsx
@@ -1,5 +1,43 @@
+import React from 'react';
+import "../../styles/css/ERDEditor.css";
+
 const ERDEditor = () => {
-  return <div>ERD 작성 화면 (추후 ERD 도구 연동 가능)</div>;
+  const handleAddTable = () => {
+    console.log('+ 테이블 추가 클릭됨');
+    // 테이블 추가 로직을 여기에 작성하세요
+  };
+
+  return (
+    <div>
+      {/* 제목 + 버튼 정렬 */}
+      <div className="erd-header">
+        <h2>ERD 작성</h2>
+        <button className="table-button" onClick={handleAddTable}>
+          + 테이블 추가
+        </button>
+      </div>
+
+      {/* 나중에 ERD 캔버스 영역이 이 아래 들어감 */}
+      <div className="erd-canvas">
+
+      </div>
+
+      <div className="erd-legend">
+        <img
+          src="/assets/icons/primary_key.svg"
+          alt="Primary"
+          className="icon"
+        />
+        <span>Primary Key</span>
+        <img
+          src="/assets/icons/foreign_key.svg"
+          alt="Foreign"
+          className="icon"
+        />
+        <span>Foreign Key</span>
+      </div>
+    </div>
+  );
 };
 
 export default ERDEditor;
