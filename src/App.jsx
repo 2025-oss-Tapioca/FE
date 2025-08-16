359a67418bd56f146a0c678404a1967e6164d037
cyyoon0311 <cy_0311@naver.com>
2025-08-16 21:23:42 +0900

    feat/#34 팀 페이지 api 함수명 수정, 홈 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import Team from "./pages/Team";
 import SignUp from "./pages/SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/team/:teamCode" element={<Home />} />
         <Route path="/team" element={<Team />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/signup" element={<SignUp />} />
